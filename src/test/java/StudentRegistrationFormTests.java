56e41320de8016a5fb3d74a79222291a3bfb6eb5
Dmitry Ivanov <ivanov.dmtr.iv@gmail.com>
2026-06-08 23:08:21 +0500

    test: add positive test for registration form

--- src/test/java/StudentRegistrationFormTests.java
+++ src/test/java/StudentRegistrationFormTests.java
@@ -1,15 +1,20 @@
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.io.File;
 import java.time.LocalDate;
 
+import static com.codeborne.selenide.Condition.matchText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class StudentRegistrationFormTests extends TestBase {
 
     @Test
-    void addDataTest() {
+    void shouldSubmitWithValidData() {
         open("/automation-practice-form");
 
         $("[id=firstName]").setValue("John");
@@ -18,59 +23,32 @@ public class StudentRegistrationFormTests extends TestBase {
         $("#gender-radio-1").click();
         $("#userNumber").setValue("7479990002");
         $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("2000");
-        $("body").click();
+        $(".react-datepicker__day--018").click();
         $("#subjectsInput").setValue("Maths").pressEnter();
-//        $("#hobbies-checkbox-1").click();
-//
-//        $("#currentAddress").setValue("Проснись, John Doe, ты надежда мира!\n" +
-//                "Я понял, что всё это время не у того просил\n" +
-//                "Собака больше не хочет корма, все спокойно, хозяева живы, пока\n" +
-//                "Случайно порезанный палец и кровь не напомнят щенку, что он сын волка");
+        $("#hobbies-checkbox-1").click();
+//        $("#uploadPicture").uploadFile(new File("src/test/resources/client_rights_on_red.png"));
+        $("#uploadPicture").uploadFromClasspath("client_rights_on_red.png");
+        $("#currentAddress").setValue("Проснись, John Doe, ты надежда мира!\n" +
+                "Я понял, что всё это время не у того просил\n" +
+                "Собака больше не хочет корма, все спокойно, хозяева живы, пока\n" +
+                "Случайно порезанный палец и кровь не напомнят щенку, что он сын волка");
+        $("#react-select-3-input").setValue("Haryana").pressEnter();
+        $("#react-select-4-input").setValue("Karnal").pressEnter();
+        $("#submit").click();
+        //        System.out.println($$("tbody tr").texts());
+        $$("tbody tr").findBy(text("Student Name")).shouldHave(text("John Doe"));
+//        $$("tbody tr").findBy(text("Student Name")).shouldHave(text("Male")); // test fail check
+        $$("tbody tr").findBy(text("Student Email")).shouldHave(text("some-male@example.com"));
+        $$("tbody tr").findBy(text("Gender")).shouldHave(text("Male"));
+        $$("tbody tr").findBy(text("Mobile")).shouldHave(text("7479990002"));
+        $$("tbody tr").findBy(text("Date of Birth")).shouldHave(text("18 July,2000"));
+        $$("tbody tr").findBy(text("Subjects")).shouldHave(text("Maths"));
+        $$("tbody tr").findBy(text("Hobbies")).shouldHave(text("Sports"));
+        $$("tbody tr").findBy(text("Picture")).shouldHave(text("client_rights_on_red.png"));
+        $$("tbody tr").findBy(text("Address")).shouldHave(matchText("Проснись, John Doe.*, все спокойно,.*,что он сын волка"));
+        $$("tbody tr").findBy(text("State and City")).shouldHave(text("Haryana Karnal"));
     }
 
-//    @Test
-//    void wrongPasswordAuthorizationByEntTest() {
-//        open("https://qa-guru.github.io/one-page-form/login.html");
-//
-//        $("[data-testid=login-input]").setValue("user1");
-//        $("[data-testid=password-input]").setValue("WRONG PASSWORD").pressEnter();
-//        $("[data-testid=error-message]").shouldHave(text("Wrong login or password"));
-//    }
-//
-//    @Test
-//    void emptyDataTest() {
-//        open("https://qa-guru.github.io/one-page-form/login.html");
-//        $("[data-testid=login-input]").setValue("");
-//        $("[data-testid=password-input]").setValue("");
-//        $("[data-testid=submit-button]").click();
-//        $("[data-testid=error-message]").shouldHave(text("Login and password are required (minimum 3 and 6 characters)"));
-//    }
-//
-//    @Test
-//    void emptyLoginTest() {
-//        open("https://qa-guru.github.io/one-page-form/login.html");
-//        $("[data-testid=login-input]").setValue("");
-//        $("[data-testid=password-input]").setValue("salam");
-//        $("[data-testid=submit-button]").click();
-//        $("[data-testid=error-message]").shouldHave(text("Login is required (minimum 3 characters)"));
-//    }
-//
-//    @Test
-//    void emptyPassTest() {
-//        open("https://qa-guru.github.io/one-page-form/login.html");
-//        $("[data-testid=login-input]").setValue("sabr");
-//        $("[data-testid=password-input]").setValue("");
-//        $("[data-testid=submit-button]").click();
-//        $("[data-testid=error-message]").shouldHave(text("Password is required (minimum 6 characters)"));
-//    }
-//
-//    @Test
-//    void shortPassTest() {
-//        open("https://qa-guru.github.io/one-page-form/login.html");
-//        $("[data-testid=login-input]").setValue("sabr");
-//        $("[data-testid=password-input]").setValue("");
-//        $("[data-testid=submit-button]").click();
-//        $("[data-testid=error-message]").shouldHave(text("Password is required (minimum 6 characters)"));
-//    }
 }
