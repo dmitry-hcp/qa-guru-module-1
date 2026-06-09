3ff62bc4976a314fb3910f06b15b8fe299c35841
Dmitry Ivanov <ivanov.dmtr.iv@gmail.com>
2026-06-09 23:16:33 +0500

    test: add negative test for student form

--- src/test/java/StudentRegistrationFormTests.java
+++ src/test/java/StudentRegistrationFormTests.java
@@ -4,7 +4,6 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
-
 public class StudentRegistrationFormTests extends TestBase {
 
     @Test
@@ -41,7 +40,7 @@ public class StudentRegistrationFormTests extends TestBase {
         $$("tbody tr").findBy(text("Subjects")).shouldHave(text("Maths"));
         $$("tbody tr").findBy(text("Hobbies")).shouldHave(text("Sports"));
         $$("tbody tr").findBy(text("Picture")).shouldHave(text("client_rights_on_red.png"));
-        $$("tbody tr").findBy(text("Address")).shouldHave(matchText("Проснись, John Doe.*, все спокойно,.*,что он сын волка"));
+        $$("tbody tr").findBy(text("Address")).shouldHave(matchText("Проснись, John Doe.*, все спокойно,.*, что он сын волка"));
         $$("tbody tr").findBy(text("State and City")).shouldHave(text("Haryana Karnal"));
     }
     @Test
@@ -59,11 +58,86 @@ public class StudentRegistrationFormTests extends TestBase {
         $$("tbody tr").findBy(text("Student Email")).$("td", 1).shouldBe(empty);
         $$("tbody tr").findBy(text("Gender")).shouldHave(text("Male"));
         $$("tbody tr").findBy(text("Mobile")).shouldHave(text("7479990002"));
-        $$("tbody tr").findBy(text("Date of Birth")).shouldHave(text("08 June,2026"));
+        $$("tbody tr").findBy(text("Date of Birth")).shouldNotBe(empty);
         $$("tbody tr").findBy(text("Subjects")).$("td", 1).shouldBe(empty);
         $$("tbody tr").findBy(text("Hobbies")).$("td", 1).shouldBe(empty);
         $$("tbody tr").findBy(text("Picture")).$("td", 1).shouldBe(empty);
         $$("tbody tr").findBy(text("Address")).$("td", 1).shouldBe(empty);
         $$("tbody tr").findBy(text("State and City")).$("td", 1).shouldBe(empty);
+    }
+
+    @Test
+    void shouldNotSubmitWithoutFirstName() {
+        open("/automation-practice-form");
+
+        $("#lastName").setValue("Doe");
+        $("#gender-radio-1").click();
+        $("#userNumber").setValue("7479990002");
+        $("#submit").click();
+        $(".modal-content").shouldNot(exist);
+        $("#firstName").shouldHave(attribute("required"));
+        $("#firstName").shouldBe(empty);
+    }
+
+    @Test
+    void shouldNotSubmitWithoutLastName() {
+        open("/automation-practice-form");
+
+        $("#firstName").setValue("John");
+        $("#gender-radio-1").click();
+        $("#userNumber").setValue("7479990002");
+        $("#submit").click();
+        $(".modal-content").shouldNot(exist);
+        $("#lastName").shouldHave(attribute("required"));
+        $("#lastName").shouldBe(empty);
+    }
+
+    @Test
+    void shouldNotSubmitWithoutGender() {
+        open("/automation-practice-form");
+
+        $("#firstName").setValue("John");
+        $("#lastName").setValue("Doe");
+        $("#userNumber").setValue("7479990002");
+        $("#submit").click();
+//        System.out.println($("#gender-radio-1").getAttribute("checked"));
+//        System.out.println($("#gender-radio-2").getAttribute("checked"));
+//        System.out.println($("#gender-radio-3").getAttribute("checked"));
+        $(".modal-content").shouldNot(exist);
+        $("#gender-radio-1").shouldHave(attribute("required"));
+        $("#gender-radio-2").shouldHave(attribute("required"));
+        $("#gender-radio-3").shouldHave(attribute("required"));
+        $("#gender-radio-1").shouldNot(selected);
+        $("#gender-radio-2").shouldNot(selected);
+        $("#gender-radio-3").shouldNot(selected);
+    }
+
+    @Test
+    void shouldNotSubmitWithoutMobileNumber() {
+        open("/automation-practice-form");
+
+        $("#firstName").setValue("John");
+        $("#lastName").setValue("Doe");
+        $("#gender-radio-1").click();
+        $("#submit").click();
+        $(".modal-content").shouldNot(exist);
+        $("#userNumber").shouldHave(attribute("required"));
+        $("#userNumber").shouldBe(empty);
+//        System.out.println($("#userNumber").getAttribute("required"));
+//        System.out.println($("#subjectsInput").getAttribute("required"));
+    }
+
+    @Test
+    void shouldNotSubmitWithWrongMobileNumber() {
+        open("/automation-practice-form");
+
+        $("#firstName").setValue("John");
+        $("#lastName").setValue("Doe");
+        $("#gender-radio-1").click();
+        $("#userNumber").setValue("+774799900");
+        $("#submit").click();
+        $(".modal-content").shouldNot(exist);
+        $("#userNumber").shouldHave(attribute("required"));
+        $("#userNumber").shouldHave(value("+774799900"));
     }
 }
